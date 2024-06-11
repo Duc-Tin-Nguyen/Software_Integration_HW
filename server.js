9cd54d4556eb1b904f236e37e9977755725426e2
Duc-Tin-Nguyen <tinnd1506@gmail,com>
2024-06-11 15:24:06 +0200

    Final changes

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.json());
 // Define routes
 app.use('/api/items', require('./routes/itemRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
