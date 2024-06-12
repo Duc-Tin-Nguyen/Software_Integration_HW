5cbdc2dac028ccd21aefb81f00ffd1c55bfa28c2
Duc-Tin-Nguyen <tinnd1506@gmail,com>
2024-06-12 16:40:30 +0200

    Created App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ItemForm from './components/ItemForm';
+import ItemList from './components/ItemList';
 
 function App() {
+  const handleItemCreated = (newItem) => {
+    // Handle the newly created item, such as updating state or performing other actions
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Item Management</h1>
+      <ItemForm onItemCreated={handleItemCreated} />
+      <h2>Items</h2>
+      <ItemList />
     </div>
   );
 }
