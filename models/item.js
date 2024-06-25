cf265cefe49a4bdfc87b93de9b787a4da79356bb
Duc-Tin-Nguyen <tinnd1506@gmail,com>
2024-06-25 18:41:09 +0200

    feat: add new feature

--- models/item.js
+++ models/item.js
@@ -5,6 +5,8 @@ const ItemSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     date: { type: Date, default: Date.now }
+
 });
+
 
 module.exports = mongoose.model('Item', ItemSchema);
